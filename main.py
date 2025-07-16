bea89e05d19f02dd3c1f9ea43b14c1b6a34dc6da
dalioumaima <107401128+dalioumaima@users.noreply.github.com>
2025-07-16 11:43:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1078,6 +1078,6 @@ def serve_react(path):
 
 
 if __name__ == "__main__":
-    
-    port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
